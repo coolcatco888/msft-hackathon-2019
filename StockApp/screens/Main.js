ed070e8c3023e7d0d59ed7cffd86eeda57ecd9af
coolcatco888 <coolcatco888@hotmail.com>
2019-07-22 19:10:26 -0700

    Created functioning clinet

--- StockApp/screens/Main.js
+++ StockApp/screens/Main.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, FlatList } from 'react-native';
 import { SearchBar } from 'react-native-elements';
+import StockClient from '../clients/StockClient';
 
 // This class is the main screen
+const stockClient = new StockClient();
+
 export default class Main extends Component {
-    state = {
-        search: '',
-    };
+
+    constructor() {
+        super();
+        this.state = {
+            searchState: {
+                search: ''
+            },
+            loading: false,
+            stockList: []
+        };
+    }
 
     updateSearch = search => {
-        this.setState({ search });
+        this.setState({
+            loading: true,
+            searchState: { search: search }
+        });
+        stockClient.search(search).then(
+            (data) => {
+                if (search != null && search != '') {
+                    this.setState({
+                            loading: false,
+                            stockList: data 
+                        });
+                }
+            }
+        )
     };
 
     render() {
-        const { search } = this.state;
+        const { search } = this.state.searchState;
 
         return (
             <View style={MainStyles.container}>
@@ -39,6 +63,11 @@ export default class Main extends Component {
                     />
                 </View>
                 <View style={MainStyles.stocklist}>
+                    <FlatList
+                        data={this.state.stockList}
+                        renderItem={({item}) => <Text>{item.symbol}</Text>}
+                        keyExtractor={item => item.symbol}
+                    />
                 </View>
             </View>
         );
